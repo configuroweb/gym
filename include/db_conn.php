e72dd56c64b65641782a80f49b189cab4246c17f
Roani Campos <jcamposg15@unc.edu.pe>
2023-06-13 11:41:55 -0500

    Se creo la BD y se modifico la conexión

--- include/db_conn.php
+++ include/db_conn.php
@@ -8,7 +8,7 @@ $db_name  = "titangym"; // Database name
 $con = mysqli_connect($host, $username, $password, $db_name);
 
 // Check connection
-if (mysqli_connect_errno($con)) {
+if (mysqli_errno($con)) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 ?>
